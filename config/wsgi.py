6c0645e3eecf7877a84fabcc92d4b4a6dae3a95c
= <=>
2015-09-17 17:27:33 -0400

    changed wigs back

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,23 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-#import os
-
-#from django.core.wsgi import get_wsgi_application
-#from whitenoise.django import DjangoWhiteNoise
-
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
-#application = get_wsgi_application()
-#application = DjangoWhiteNoise(application)
-
-
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prompt.settings")
-
 from whitenoise.django import DjangoWhiteNoise
 
-application = DjangoWhiteNoise(get_wsgi_application())
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
+
+#import os
+
+#from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prompt.settings")
+
+#from whitenoise.django import DjangoWhiteNoise
+
+#application = DjangoWhiteNoise(get_wsgi_application())
