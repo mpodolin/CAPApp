f6e4f067bcac6a682e90cabe212e963b5e4b1248
= <=>
2015-09-17 16:21:53 -0400

    Changed static root and static files dirs

--- config/settings.py
+++ config/settings.py
@@ -112,9 +112,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
 )
 
 AUTH_USER_MODEL = 'promotions.Cadet'
